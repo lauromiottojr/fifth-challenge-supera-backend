cedd309b3e3edccf19f72b3e5fb2a5c1e009f231
lauromiottojr <junior.miotto36@gmail.com>
2022-12-20 22:49:08 -0300

    Fix CORS

--- src/main/java/br/com/banco/controllers/TransactionController.java
+++ src/main/java/br/com/banco/controllers/TransactionController.java
@@ -3,6 +3,7 @@ package br.com.banco.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,7 @@ public class TransactionController {
 	@Autowired
 	private TransactionService transactionService;
 
+	@CrossOrigin(origins = "http://localhost:8080")
 	@GetMapping
 	public List<TransactionModel> findTransactions() {
 		return transactionService.findTransactions();
